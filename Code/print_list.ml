c076b1de387f7363fb1ca6df829bbd05e835052b
Mikey Ferguson <90787330+Pirate-Hunter-Zoro@users.noreply.github.com>
2024-05-05 18:34:43 -0500

    rough draft list methods implemented

--- Code/print_list.ml
+++
@@ -1,4 +0,0 @@
-let rec print l =
-  match l with
-  | [] -> print_string "\n"
-  | h::t -> print_string h; print t;;
--- Code/reverse_list.ml
+++
@@ -1,7 +0,0 @@
-let reverse l = 
-  rev rec l [];;
-
-let rev rec l acc =
-  match l with
-  | [] -> acc
-  | h::t -> rec t h::acc
